bf9e83a4b82bd39b6442aff84ff63332acd4f097
viktormelker <viktor.melker@fyndiq.com>
2017-05-11 16:02:29 +0200

    Updated progam to use neural network solution

--- cartpole_v0_nn.py
+++ cartpole_v0_nn.py
@@ -1,12 +1,56 @@
 import gym
+import numpy as np
+import tensorflow as tf
+
 env = gym.make('CartPole-v0')
-for i_episode in range(20):
-    observation = env.reset()
-    for t in range(100):
-        env.render()
-        print(observation)
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
-        if done:
-            print("Episode finished after {} timesteps".format(t+1))
-            break
+tf.reset_default_graph()
+
+#These lines establish the feed-forward part of the network used to choose actions
+inputs1 = tf.placeholder(shape = [1, 4], dtype = tf.float32)
+W = tf.Variable(tf.random_uniform([4, 2], 0, 0.01))
+Qout = tf.matmul(inputs1, W)
+predict = tf.argmax(Qout, 1)
+
+#Below we obtain the loss by taking the sum of squares difference between the target and prediction Q values.
+nextQ = tf.placeholder(shape = [1, 2], dtype = tf.float32)
+loss = tf.reduce_sum(tf.square(nextQ - Qout))
+trainer = tf.train.AdamOptimizer(learning_rate = 0.01)
+updateModel = trainer.minimize(loss)
+
+init = tf.initialize_all_variables()
+
+# Set learning parameters
+randomness_probability = 0.1
+num_episodes = 50
+max_time = 300
+
+# create lists to contain total rewards and steps per episode
+jList = []
+rList = []
+with tf.Session() as sess:
+    sess.run(init)
+    for i_episode in range(num_episodes):
+        observation = env.reset()
+        rAll = 0
+        done = False
+        current_step = 0
+
+        while current_step < max_time:
+            current_step += 1
+            
+            input = np.reshape(observation, (1, 4))
+            # Choose an action by greedily (with randomness_probability chance
+            # of random action) from the Q-network
+            action, allQ = sess.run([predict, Qout],
+                                    feed_dict={inputs1: input})
+            if np.random.rand(1) < randomness_probability:
+                action[0] = env.action_space.sample()
+
+            # Get new state and reward from environment
+            observation, reward, done, _ = env.step(action[0])
+            env.render()
+
+            if done:
+                print("Episode finished after {} timesteps".format(
+                    current_step + 1))
+                break
