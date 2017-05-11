361427dd82053b36bf7e2fdf2f6e5d8deb822639
viktormelker <viktor.melker@fyndiq.com>
2017-05-11 13:09:27 +0200

    added example with table solution

---
+++ frozen_lake_v0.py
@@ -0,0 +1,46 @@
+# taken from https://medium.com/emergent-future/simple-reinforcement-learning-with-tensorflow-part-0-q-learning-with-tables-and-neural-networks-d195264329d0
+
+import gym
+import numpy as np
+
+env = gym.make('FrozenLake-v0')
+
+print(env.action_space)
+print(env.observation_space)
+# print(gym.envs.registry.all())
+
+Q = np.zeros([env.observation_space.n, env.action_space.n])
+# Set learning parameters
+lr = .85
+y = .99
+num_episodes = 2000
+
+# create lists to contain total rewards and steps per episode
+rList = []
+
+for attempt in range(num_episodes):
+    # Reset environment and get first new observation
+    s = env.reset()
+    rAll = 0
+    done = False
+    step = 0
+    # The Q-Table learning algorithm
+    while step < 99:
+        step += 1
+        # Choose an action by greedily (with noise) picking from Q table
+        a = (np.argmax(Q[s, :] +
+            np.random.randn(1, env.action_space.n) * (1 ./ (attempt + 1))))
+        # Get new state and reward from environment
+        observation, reward, done, _ = env.step(a)
+        # Update Q-Table with new knowledge
+        Q[s, a] = Q[s, a] + lr * (reward + y * np.max(Q[observation, :]) - Q[s, a])
+
+        rAll += reward
+        s = observation
+        if done is True:
+            break
+    rList.append(rAll)
+
+print "Score over time: " + str(sum(rList) / num_episodes)
+print "Final Q-Table Values"
+print Q
