1ae804be385a82885409416223c184603d1a42e2
Viktor Melker <viktormelker@gmail.com>
2019-01-19 10:07:59 +0100

    cleanup

--- frozen_lake/frozen_lake_v0_nn.py
+++ frozen_lake/frozen_lake_v0_nn.py
@@ -10,16 +10,16 @@ env = gym.make('FrozenLake-v0')
 
 tf.reset_default_graph()
 
-#These lines establish the feed-forward part of the network used to choose actions
-inputs1 = tf.placeholder(shape = [1, 16], dtype = tf.float32)
+# These lines establish the feed-forward part of the network used to choose actions
+inputs1 = tf.placeholder(shape=[1, 16], dtype=tf.float32)
 W = tf.Variable(tf.random_uniform([16, 4], 0, 0.01))
 Qout = tf.matmul(inputs1, W)
 predict = tf.argmax(Qout, 1)
 
-#Below we obtain the loss by taking the sum of squares difference between the target and prediction Q values.
-nextQ = tf.placeholder(shape = [1, 4], dtype = tf.float32)
+# Below we obtain the loss by taking the sum of squares difference between the target and prediction Q values.
+nextQ = tf.placeholder(shape=[1, 4], dtype=tf.float32)
 loss = tf.reduce_sum(tf.square(nextQ - Qout))
-trainer = tf.train.GradientDescentOptimizer(learning_rate = 0.1)
+trainer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
 updateModel = trainer.minimize(loss)
 
 init = tf.initialize_all_variables()
@@ -43,28 +43,31 @@ with tf.Session() as sess:
         while current_step < 99:
             current_step += 1
             # Choose an action by greedily (with e chance of random action) from the Q-network
-            a, allQ = sess.run([predict, Qout], feed_dict = {inputs1:np.identity(16)[s: s + 1]})
+            a, allQ = sess.run([predict, Qout], feed_dict={
+                               inputs1: np.identity(16)[s: s + 1]})
             if np.random.rand(1) < e:
                 a[0] = env.action_space.sample()
             # Get new state and reward from environment
             observation, reward, done, _ = env.step(a[0])
             # Obtain the Q' values by feeding the new state through our network
-            Q1 = sess.run(Qout, feed_dict = {inputs1:np.identity(16)[observation: observation + 1]})
+            Q1 = sess.run(Qout, feed_dict={inputs1: np.identity(16)[
+                          observation: observation + 1]})
             # Obtain maxQ' and set our target value for chosen action.
             maxQ1 = np.max(Q1)
             targetQ = allQ
             targetQ[0, a[0]] = reward + y * maxQ1
             # Train our network using target and predicted Q values
-            _,W1 = sess.run([updateModel, W],feed_dict = {inputs1:np.identity(16)[s:s + 1],nextQ:targetQ})
+            _, W1 = sess.run([updateModel, W], feed_dict={
+                             inputs1: np.identity(16)[s:s + 1], nextQ: targetQ})
             rAll += reward
             s = observation
-            if done == True:
+            if done is True:
                 # Reduce chance of random action as we train the model.
                 e = 1./((i/50) + 10)
                 break
         jList.append(current_step)
         rList.append(rAll)
-print "Percent of succesful episodes: " + str(sum(rList)/num_episodes) + "%"
+print("Percent of succesful episodes: " + str(sum(rList)/num_episodes) + "%")
 
 plt.plot(rList)
 plt.plot(jList)
