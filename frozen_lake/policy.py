1340e7c0aa3c7457b9ed1483a1bbb832a056c800
Viktor Melker <viktormelker@gmail.com>
2019-01-20 20:30:00 +0100

    fix bug with max value

--- frozen_lake/policy.py
+++ frozen_lake/policy.py
@@ -15,13 +15,17 @@ class Policy:
     def update(self, states, actions, reward):
         age = len(states) - 1
         for state, action in zip(states, actions):
-            self.action_probabilities[state, action] += max(0, (
+            self.action_probabilities[state, action] = max(0, (
+                self.action_probabilities[state, action] +
                 self.learning_rate * reward * pow(self.age_factor, age)
             ))
 
-            self.action_probabilities[state] = (
-                self.action_probabilities[state] / self.action_probabilities[state].sum())
+            self._normalize(state)
             age -= 1
+
+    def _normalize(self, state):
+        self.action_probabilities[state] = (
+            self.action_probabilities[state] / self.action_probabilities[state].sum())
 
     def get_action(self, state):
         return np.random.choice(
