ad01bee3a2fb7579d7cb622c41d797b91f6379f8
Daniel Klevebring <daniel.klevebring@gmail.com>
2019-01-21 16:47:54 +0100

    refactor Q update + lint

--- frozen_lake/policy.py
+++ frozen_lake/policy.py
@@ -14,7 +14,6 @@ class BasePolicy:
 
 
 class SimplePolicy(BasePolicy):
-
     def __init__(self, num_actions, num_states):
         super().__init__(num_actions, num_states)
         self.learning_rate = 0.1
@@ -28,26 +27,24 @@ class SimplePolicy(BasePolicy):
 
         age = len(states) - 1
         for state, action in zip(states, actions):
-            self.action_probabilities[state, action] = max(0, (
-                self.action_probabilities[state, action] +
-                self.learning_rate * reward * pow(self.age_factor, age)
-            ))
+            self.action_probabilities[state, action] = max(
+                0, (self.action_probabilities[state, action] +
+                    self.learning_rate * reward * pow(self.age_factor, age)))
 
             self._normalize(state)
             age -= 1
 
     def _normalize(self, state):
         self.action_probabilities[state] = (
-            self.action_probabilities[state] / self.action_probabilities[state].sum())
+            self.action_probabilities[state] /
+            self.action_probabilities[state].sum())
 
     def get_action(self, state, attempt=1):
         return np.random.choice(
-            self.num_actions, 1, p=self.action_probabilities[state]
-        )[0]
+            self.num_actions, 1, p=self.action_probabilities[state])[0]
 
 
 class QTablePolicy(BasePolicy):
-
     def __init__(self, num_actions, num_states):
         super().__init__(num_actions, num_states)
         self.Q = np.zeros([num_states, num_actions])
@@ -57,12 +54,17 @@ class QTablePolicy(BasePolicy):
     def update(self, states, actions, reward, result_state, **kwargs):
         state = states[-1]
         action = actions[-1]
+
         # Update Q-Table with new knowledge from last step
         self.Q[state, action] = (
-            self.Q[state, action] + self.learning_rate *
-            (reward + self.y * np.max(self.Q[result_state, :]) - self.Q[state, action])
-        )
+            (1 - self.learning_rate) * self.Q[state, action] +
+            self.learning_rate *
+            (reward + self.y * np.max(self.Q[result_state, :])))
+
+        return
 
     def get_action(self, state, attempt, **kwargs):
         # Choose an action by greedily (with noise) picking from Q table
-        return (np.argmax(self.Q[state, :] + np.random.randn(1, self.num_actions) * (1./(attempt + 1))))
+        return (
+            np.argmax(self.Q[state, :] + np.random.randn(1, self.num_actions) *
+                      (1. / (attempt + 1))))
