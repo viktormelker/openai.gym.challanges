1a727f321f235ebf532e3b66bc1911f7b80a4b24
Daniel Klevebring <daniel.klevebring@gmail.com>
2019-01-21 16:48:12 +0100

    remove time_reward or total_reward

--- frozen_lake/frozen_lake_v0_q_learning.py
+++ frozen_lake/frozen_lake_v0_q_learning.py
@@ -11,13 +11,11 @@ env = gym.make('FrozenLake-v0')
 num_episodes = 10000
 max_steps = 100
 reward_queue = deque()
-time_reward = 0
 
 policy = QTablePolicy(num_actions=env.action_space.n,
                       num_states=env.observation_space.n)
 
 for attempt in range(num_episodes):
-    total_reward = 0
     state = env.reset()
     states = []
     actions = []
@@ -28,13 +26,9 @@ for attempt in range(num_episodes):
         actions.append(action)
 
         state, reward, done, _ = env.step(action)
-        reward += time_reward
-
-        total_reward += reward
-        policy.update(states, actions, total_reward, result_state=state)
+        policy.update(states, actions, reward, result_state=state)
 
         if done is True:
-            reward_queue.append(total_reward)
             break
 
 print("Score over time: " + str(sum(reward_queue) / num_episodes))
