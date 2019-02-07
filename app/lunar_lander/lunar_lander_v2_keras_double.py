9c78f67a7b107e7e23e5a2c30140edd0099be422
Viktor Melker <viktormelker@gmail.com>
2019-02-07 23:11:40 +0100

    Add storage of weights between runs

--- app/lunar_lander/lunar_lander_v2_keras_double.py
+++ app/lunar_lander/lunar_lander_v2_keras_double.py
@@ -4,16 +4,18 @@ import numpy as np
 from app.policy import DoubleDQNAgent
 from collections import deque
 
-EPISODES = 200
+EPISODES = 25
 
 if __name__ == "__main__":
     # initialize gym environment and the agent
     env = gym.make('LunarLander-v2')
     state_size = env.observation_space.shape[0]
     agent = DoubleDQNAgent(
-        state_size=state_size, action_size=env.action_space.n, gamma=0.98)
+        state_size=state_size, action_size=env.action_space.n,
+        gamma=0.99, epsilon_min=0.01, weight_file='app/lunar_lander/weights/DQNN_weights.h5')
+    time_reward = -0.25
 
-    total_rewards = deque(maxlen=20)
+    total_rewards = deque(maxlen=10)
     # Iterate the game
     for episode in range(EPISODES):
         # reset state in the beginning of each game
@@ -23,16 +25,16 @@ if __name__ == "__main__":
         rewards = []
         for time_t in range(500):
             # turn this on if you want to render
-            if episode > 20:
+            if episode > 30:
                 env.render()
 
             # Decide action
             action = agent.act(state)
 
             next_state, reward, done, _ = env.step(action)
+            reward += time_reward
 
             next_state = np.reshape(next_state, [1, state_size])
-
 
             # Remember the previous state, action, reward, and done
             rewards.append(reward)
@@ -54,3 +56,5 @@ if __name__ == "__main__":
         print(
             "episode: {}/{}, time_t: {}, reward: {}, average reward: {}".format(
                 episode, EPISODES, time_t, total_reward, sum(total_rewards)/len(total_rewards)))
+
+    agent.save_weights()
