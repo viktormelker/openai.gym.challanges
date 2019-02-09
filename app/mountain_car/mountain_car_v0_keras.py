0d1689a297fc5a71d2bd3bf1b12766f14cce5a35
Viktor Melker <viktormelker@gmail.com>
2019-02-09 15:44:52 +0100

    Refactor mountain_car

--- app/mountain_car/mountain_car_v0_keras.py
+++ app/mountain_car/mountain_car_v0_keras.py
@@ -1,61 +1,65 @@
-import gym
 import numpy as np
 
-from app.policy import DQNAgent
+from app.mountain_car.agents import dqnAgent as agent
+from app.mountain_car.environment import env, success_x_pos
 from collections import deque
 
 EPISODES = 1000
 
 if __name__ == "__main__":
-    # initialize gym environment and the agent
-    env = gym.make("MountainCar-v0")
-    # MountainCarContinuous-v0
     state_size = env.observation_space.shape[0]
-    agent = DQNAgent(state_size=state_size, action_size=env.action_space.n)
 
     best_positions = deque(maxlen=20)
-    # Iterate the game
-    for e in range(EPISODES):
-        # reset state in the beginning of each game
+
+    for episode in range(EPISODES):
+
         state = env.reset()
         state = np.reshape(state, [1, state_size])
-        # time_t represents each frame of the game
-        # Our goal is to keep the pole upright as long as possible until score of 500
-        # the more time_t the more score
+
         rewards = []
         positions = [-100]
         for time_t in range(500):
-            # turn this on if you want to render
-            # env.render()
-            # Decide action
+
+            if episode > 20:
+                env.render()
+
             action = agent.act(state)
-            # Advance the game to the next frame based on the action.
 
             next_state, reward, done, _ = env.step(action)
 
             next_state = np.reshape(next_state, [1, state_size])
 
+            if next_state[0, 0] >= 0.5:
+                reward = 1000  # we did it!
             if next_state[0, 0] > max(positions):
                 reward = next_state[0, 0]
 
             positions.append(next_state[0, 0])
 
             # Remember the previous state, action, reward, and done
-            if reward:
-                agent.remember(state, action, reward, next_state, done)
-            # make next_state the new current state for the next frame.
+            agent.remember(state, action, reward, next_state, done)
+
+            agent.replay()
+
             state = next_state
-            # done becomes True when the game ends
-            # ex) The agent drops the pole
+
             if done:
                 # print the score and break out of the loop
                 best_positions.append(max(positions))
+                average_best_pos = sum(best_positions) / len(best_positions)
 
                 print(
-                    "episode: {}/{}, time_t: {}, max pos: {}".format(
-                        e, EPISODES, time_t, max(positions)
+                    "episode: {0:4d}/{1:4d}, time_t: {2:3d}, max pos: {3:8.4f}, avg pos: {4:8.4f}".format(
+                        episode,
+                        EPISODES,
+                        time_t,
+                        max(positions),
+                        sum(best_positions) / len(best_positions),
                     )
                 )
                 break
-        # train the agent with the experience of the episode
-        agent.replay(2000)
+
+        if average_best_pos > success_x_pos:
+            print(
+                f'Successfully finished the challenge in {episode} training runs!')
+            break
--- app/mountain_car/mountain_car_v0_keras_double.py
+++ app/mountain_car/mountain_car_v0_keras_double.py
@@ -1,31 +1,28 @@
 import numpy as np
 
 from app.mountain_car.agents import ddqnAgent as agent
-from app.mountain_car.environment import env
+from app.mountain_car.environment import env, success_x_pos
 from collections import deque
 
 EPISODES = 1000
 
 if __name__ == "__main__":
-
     state_size = env.observation_space.shape[0]
+
     best_positions = deque(maxlen=20)
-    # Iterate the game
+
     for episode in range(EPISODES):
-        # reset state in the beginning of each game
+
         state = env.reset()
         state = np.reshape(state, [1, state_size])
-        # time_t represents each frame of the game
-        # Our goal is to keep the pole upright as long as possible until score of 500
-        # the more time_t the more score
+
         rewards = []
         positions = [-100]
         for time_t in range(500):
-            # turn this on if you want to render
+
             if episode > 20:
                 env.render()
 
-            # Decide action
             action = agent.act(state)
 
             next_state, reward, done, _ = env.step(action)
@@ -44,13 +41,12 @@ if __name__ == "__main__":
             agent.replay()
             agent.target_train()
 
-            # make next_state the new current state for the next frame.
             state = next_state
-            # done becomes True when the game ends
-            # ex) The agent drops the pole
+
             if done:
                 # print the score and break out of the loop
                 best_positions.append(max(positions))
+                average_best_pos = sum(best_positions) / len(best_positions)
 
                 print(
                     "episode: {0:4d}/{1:4d}, time_t: {2:3d}, max pos: {3:8.4f}, avg pos: {4:8.4f}".format(
@@ -58,7 +54,12 @@ if __name__ == "__main__":
                         EPISODES,
                         time_t,
                         max(positions),
-                        sum(best_positions) / len(best_positions),
+                        average_best_pos,
                     )
                 )
                 break
+
+        if average_best_pos > success_x_pos:
+            print(
+                f'Successfully finished the challenge in {episode} training runs!')
+            break
