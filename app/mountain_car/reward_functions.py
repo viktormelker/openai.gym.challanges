68b1816e6448a58c01b758e1b9c983b7bf62ee90
Viktor Melker <viktormelker@gmail.com>
2019-02-10 01:46:42 +0100

    remove comments

--- app/mountain_car/reward_functions.py
+++ app/mountain_car/reward_functions.py
@@ -4,6 +4,4 @@ scaling_factor = 1
 def potential_function(state):
     x_pos = state[0][0]
     speed = state[0][1]
-    # print(f'speed: {speed:4.2f}, xpos: {x_pos:4.2f}')
-    # return ((speed * speed / 2) + x_pos * 5) * scaling_factor
     return x_pos * 100 + speed * speed * 10
