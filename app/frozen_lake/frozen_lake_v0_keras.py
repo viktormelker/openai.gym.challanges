142099fb27f2b7a34d333f21632d36dd01dd5072
Viktor Melker <viktormelker@gmail.com>
2019-02-10 17:37:19 +0100

    Adapt to new agents

--- app/frozen_lake/frozen_lake_v0_keras.py
+++ app/frozen_lake/frozen_lake_v0_keras.py
@@ -7,15 +7,15 @@ EPISODES = 1000
 
 if __name__ == "__main__":
     # initialize gym environment and the agent
-    env = gym.make("CartPole-v0")
+    env = gym.make("FrozenLake-v0")
     agent = DQNAgent(
-        state_size=env.observation_space.shape[0], action_size=env.action_space.n
+        state_size=env.observation_space.n, action_size=env.action_space.n
     )
     # Iterate the game
     for e in range(EPISODES):
         # reset state in the beginning of each game
         state = env.reset()
-        state = np.reshape(state, [1, 4])
+        state = np.reshape(state, [1, 1])
         # time_t represents each frame of the game
         # Our goal is to keep the pole upright as long as possible until score of 500
         # the more time_t the more score
@@ -27,7 +27,7 @@ if __name__ == "__main__":
             # Advance the game to the next frame based on the action.
             # Reward is 1 for every frame the pole survived
             next_state, reward, done, _ = env.step(action)
-            next_state = np.reshape(next_state, [1, 4])
+            next_state = np.reshape(next_state, [1, 1])
             # Remember the previous state, action, reward, and done
             agent.update(state, action, reward, next_state, done)
             # make next_state the new current state for the next frame.
