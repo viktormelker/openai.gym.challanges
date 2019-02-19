215e817011ce255dedb8e0b546fbc92eddd87aa3
Viktor Melker <viktormelker@gmail.com>
2019-02-19 16:27:57 +0100

    Add setup script

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Openaisolver",
+    version="0.1.0",
+    description="Openai Gym solver functions",
+    url="https://github.com/viktormelker/openai.gym.challanges",
+    author="Viktor Melker",
+    author_email="",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=["keras>=2.2.4", "h5py>=2.9.0"],
+    zip_safe=False,
+)
