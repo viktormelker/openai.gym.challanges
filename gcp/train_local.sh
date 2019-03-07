12ed82924b90957a40ba899d3083d0e564485afb
Viktor Melker <viktormelker@gmail.com>
2019-03-07 17:16:03 +0100

    cleanup

--- gcp/train_local.sh
+++ gcp/train_local.sh
@@ -1,7 +1,3 @@
-JOBNAME=rl_job_hp_$(date -u +%y%m%d_%H%M%S)
-REGION=europe-west1
-BUCKET=viktor_rl_test_bucket
-
 gcloud ml-engine local train --module-name app.run_lunar_lander \
           --package-path=$PWD/app \
           --distributed \
